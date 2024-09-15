bdbfa7e2357437e862c99866a77bd65da33a89aa
Rupak <rupakkhadka03@gmail.com>
2024-09-15 20:32:02 +0545

    Simple Compress GUI

--- functions.py
+++ functions.py
@@ -1,9 +1,12 @@
-def get_todos(filepath):
+
+FILEPATH="todos.txt"
+
+def get_todos(filepath=FILEPATH):
     with open(filepath, 'r') as file_local:
         newtodo = file_local.readlines()
         return newtodo
 
 
-def write_todos(todos_arg, filepath):
+def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
